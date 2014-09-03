d3e62a678b9bd7811997f7c3861918822fd3695c
Timon <timon.langlotz@gmail.com>
2014-09-03 14:55:52 +0200

    added maxInt and removeFst

--- timon-langlotz/Sol1.hs
+++ timon-langlotz/Sol1.hs
@@ -1,3 +1,13 @@
 module Sol1 where
 
 import GS
+
+maxInt :: [Int] -> Int
+maxInt [] = error "empty list" 
+maxInt [x] = x
+maxInt (x:xs) = max x (maxInt xs)
+
+removeFst :: Int -> [Int] -> [Int]
+removeFst _ [] 					= []
+removeFst m (x:xs) 	| m == x 	= xs
+					| otherwise = x:removeFst xs
