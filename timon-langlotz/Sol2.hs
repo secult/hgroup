5b89c96b96f343654a9358313891ee72104ee840
Timon <timon.langlotz@gmail.com>
2014-09-05 22:26:46 +0200

    finished excercises 1 and 2 from week1

--- timon-langlotz/Sol2.hs
+++ timon-langlotz/Sol2.hs
@@ -2,3 +2,32 @@ module Sol2 where
 
 import GS
 import TAMO
+
+-- Excercise 2.51
+unique :: (a -> Bool) -> [a] -> Bool
+unique p []		= False
+unique p (a:as) = oneTrue (map p (a:as)) False
+
+oneTrue :: [Bool] -> Bool -> Bool 
+oneTrue [] b 	| b = True
+				| otherwise = False
+oneTrue (x:xs) True | x = False
+					| otherwise = oneTrue xs True
+oneTrue (x:xs) False 	| x = oneTrue xs True
+						| otherwise = oneTrue xs False
+-- end 2.51
+
+-- Excercise 2.52
+parity :: [Bool] -> Bool
+parity xs = mod (count 0 xs) 2 == 0
+
+count :: Int -> [Bool] -> Int
+count c []					= c
+count c (x:xs)	| x 		= count (c+1) xs
+				| otherwise = count c xs
+-- end 2.52
+
+-- Excercise 2.53
+evenNR :: (a -> Bool) -> [a] -> Bool
+evenNR p as = parity (map p as)
+-- end 2.53
