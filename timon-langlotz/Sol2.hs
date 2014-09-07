6665dd5ce3169f1be4e1c9a1d287a695f6cfc025
Timon <timon.langlotz@gmail.com>
2014-09-08 00:06:12 +0200

    added some solutions for bonus assignments

--- timon-langlotz/Sol2.hs
+++ timon-langlotz/Sol2.hs
@@ -3,7 +3,14 @@ module Sol2 where
 import GS
 import TAMO
 
--- Excercise 2.51
+-- Exercise 2.13
+test1a = logEquiv1 ((\ True -> not True) (\ False -> not True))
+test1b = logEquiv1 (not False) True
+
+--test2 = logEquiv2 id (\ p -> p ==> False) (\ p -> not p)
+-- end 2.13
+
+-- Exercise 2.51
 unique :: (a -> Bool) -> [a] -> Bool
 unique p []		= False
 unique p (a:as) = oneTrue (map p (a:as)) False
@@ -17,7 +24,7 @@ oneTrue (x:xs) False 	| x = oneTrue xs True
 						| otherwise = oneTrue xs False
 -- end 2.51
 
--- Excercise 2.52
+-- Exercise 2.52
 parity :: [Bool] -> Bool
 parity xs = mod (count 0 xs) 2 == 0
 
@@ -27,7 +34,7 @@ count c (x:xs)	| x 		= count (c+1) xs
 				| otherwise = count c xs
 -- end 2.52
 
--- Excercise 2.53
+-- Exercise 2.53
 evenNR :: (a -> Bool) -> [a] -> Bool
 evenNR p as = parity (map p as)
 -- end 2.53
