138ed6aa175a9900c28e4b13b9ee6c4319f120ef
Pavol Kincel <secult@gmail.com>
2014-09-10 16:21:54 +0200

    made some more lab excercises+ sketch of a test report

--- secult/Lab2.hs
+++ secult/Lab2.hs
@@ -37,4 +37,31 @@ runTest= all demoCheck demoData
 -- time spent: another 30 min
 
 --task 2
+removeFst ::Eq a=> a -> [a]-> [a]
+removeFst m [] =[]
+removeFst m [a] | m ==a =[]
+removeFst m (x:xs) | m ==x =xs
+        | otherwise = x:(removeFst m xs)
+
 isPermutation :: Eq a => [a] -> [a] -> Bool
+-- too simple and bad for big lists - isPermutation a b = any (==b) (permutations a)
+isPermutation [] [] = True
+isPermutation xs ys | length xs /=length ys = False
+isPermutation xs (y:ys) = isPermutation (removeFst y xs) ys
+
+--task 3
+
+canResolveNonEqualLengths :: Eq a=>Num a=> ([a]->[a]->Bool) ->  Bool
+canResolveNonEqualLengths f = not (f [1] [1,2])
+--and a lot more
+
+-- I might reduce the amount of posibilities to be tested
+
+--task 4
+
+--task 5
+
+isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement [] [] = True
+isDerangement a b | not (isPermutation a b) = False
+isDerangement (x:xs) (y:ys) = foldl (\a b -> a && uncurry (/=) b) (x/=y) (zip xs ys)
