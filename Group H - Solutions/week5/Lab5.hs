e8350627b25642fc5afd00497505053794b28def
Timon <timon.langlotz@gmail.com>
2014-10-06 17:11:38 +0200

    added some solutions for exercise 6

--- Group H - Solutions/week5/Lab5.hs
+++ Group H - Solutions/week5/Lab5.hs
@@ -64,3 +64,73 @@ hasAllNumbers x = nub x == x && sum x == sum [1..9] && not (any (<= 0) x)
 inRangeAndNoDups :: [Value] -> Bool
 inRangeAndNoDups a = nub x == x && x \\ [1..9] == []
 					 where x = filter (/=0) a
+
+                     
+-- Exercise 6
+-- 1 -> easy, 10 -> hard
+type Difficulty = Float
+
+humanSolve :: Sudoku -> Node -> Difficulty
+humanSolve sol prob | (sud2grid sol) == (sud2grid $ fst prob) = 1
+                    | (sud2grid $ fst prob) /= (sud2grid $ fst logicSol) = humanSolve sol logicSol
+                    | otherwise = refute sol prob
+                        where logicSol = logicSolve prob
+                    
+                    
+logicSolve :: Node -> Node
+logicSolve (sud,con) = undefined --any map  (\ (sud',con')
+
+nakedSingle :: [Constraint] -> Sudoku -> Sudoku
+nakedSingle cs sud = foldr (\ (row,col,vs) sud' -> if (length vs == 1) 
+                                                then extend sud ((row,col),head vs)
+                                                else sud') sud cs
+                                                
+hiddenSingle :: Sudoku -> Sudoku
+hiddenSingle sud = undefined --foldr (\ [row] sud 
+
+
+hiddenSingleColumn :: Sudoku -> Sudoku
+hiddenSingleColumn sud = foldr extSingleCol sud positions
+
+
+extSingleCol :: Column -> Sudoku -> Sudoku
+extSingleCol col sud | length value == 1 = extend sud (head (getEmptyInColumn sud col), head value)
+                     | otherwise = sud
+                            where value = freeInColumn sud col
+
+getEmptyInColumn :: Sudoku -> Column -> [(Row,Column)]
+getEmptyInColumn sud col = filter (\ (r,c) -> c == col) (openPositions sud)
+
+hiddenSingleRow :: Sudoku -> Sudoku
+hiddenSingleRow = undefined
+
+getEmptyInRow :: Sudoku -> Row -> [Column]
+getEmptyInRow sud row = undefined -- filter (\ (r,c) -> r == row) (openPositions sud)
+
+hiddenSingleSubgrid :: Sudoku -> Sudoku
+hiddenSingleSubgrid = undefined
+
+getEmptyInSubgrid :: Sudoku -> (Row,Column) -> [Row]
+getEmptyInSubgrid sud sg = undefined --filter (undefined) (openPositions sud)
+
+eachSub :: [(Row,Column)]
+eachSub = [(a,b) | a <- [2,5,8], b <- [2,5,8]]
+
+refute :: Sudoku -> Node -> Difficulty
+refute sud n = undefined
+
+bruteSudoku :: Difficulty -> IO Sudoku
+bruteSudoku = undefined
+
+example1' :: Grid
+example1' = [[5,3,0,0,7,0,0,0,0],
+             [6,0,0,1,9,5,0,0,0],
+             [1,9,8,0,0,0,0,6,0],
+             [8,0,0,0,6,0,0,0,3],
+             [4,0,0,8,0,3,0,0,1],
+             [7,0,0,0,2,0,0,0,6],
+             [9,6,0,0,0,0,2,8,0],
+             [2,0,0,4,1,9,0,0,5],
+             [0,0,0,0,8,0,0,7,9]]
+
+
