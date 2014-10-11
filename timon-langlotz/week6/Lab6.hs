e7df17287a088e43215854d5ca83b7136d54564a
Timon Langlotz <timon.langlotz@gmail.com>
2014-10-11 03:02:36 +0200

    added solution 1-5

--- timon-langlotz/week6/Lab6.hs
+++ timon-langlotz/week6/Lab6.hs
@@ -20,10 +20,17 @@ carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
 -- Exercise 2
 measureSpeed :: IO ()
 measureSpeed = do 
-        x <- getRandomInt 100000
-        y <- getRandomInt 50000
-        m <- getRandomInt 10000
-        putStrLn "Test exM"
+        x <- getRandomInt 10000000000
+        y <- getRandomInt 50000000
+        m <- getRandomInt 100000
+        putStrLn "Testing for:"
+        putStr . show $ x
+        putStr "^"
+        putStr . show $ y
+        putStr " mod "
+        print m
+        
+        putStrLn "Test exM: "
         start1 <- getCurrentTime
         putStrLn $ show $ exM x y m
         end1 <- getCurrentTime
@@ -35,7 +42,40 @@ measureSpeed = do
         end2 <- getCurrentTime
         putStrLn $ show $ diffUTCTime end2 start2
 
-testInvariant x y m = (expM x y m) == (exM x y m)
+        
+test_exM (Positive x) (Positive y) (Positive m) = (expM x y m) == (exM x y m)
 
 getRandomInt :: Integer -> IO Integer
 getRandomInt n = getStdRandom (randomR (0,n))
+
+-- Exercise 3
+composites :: [Integer]
+composites = sieve2 [2..] primes
+
+sieve2 :: [Integer] -> [Integer] -> [Integer]
+sieve2 list@(n:ns) (p:ps) = (takeWhile (<p) list) ++ 
+                                if n == p
+                                then sieve2 ns ps
+                                else sieve2 (dropWhile (<=p) list) ps
+
+-- Exercise 4 + 5
+-- primeF -> steps -> number
+testFalsePrime :: Int -> [Integer] -> IO ()
+testFalsePrime steps (c:cs) = do
+                b <- primeF steps c
+                if b then 
+                    putStrLn $ show $ c
+                else
+                    testFalsePrime steps cs
+ 
+
+-- Exercise 5
+
+
+
+
+
+
+
+
+
